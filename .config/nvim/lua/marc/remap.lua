ac8169a73f20b90e9f1cf94ae90efd987e71b989
MarcSerraPeralta <marcserraperalta@gmail.com>
2025-07-26 17:31:49 +0200

    Add comments for all remaps

--- .config/nvim/lua/marc/remap.lua
+++ .config/nvim/lua/marc/remap.lua
@@ -22,27 +22,8 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 -- delete selected lines without changing buffer (so I don't loose my yanked text)
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
--- change easily between projects with tmux
-vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+-- opens a "substitute" command to find and replace the word where the cursor is
+vim.keymap.set("n", "<leader>s", [[:%s/\\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
-vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux-sessionizer -s 0 --vsplit<CR>")
-vim.keymap.set("n", "<M-H>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
-vim.keymap.set("n", "<leader>f", function()
-    require("conform").format({ bufnr = 0 })
-end)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- makes the current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
-vim.keymap.set("n", "<leader>ca", function()
-    require("cellular-automaton").start_animation("make_it_rain")
-end)
-
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
