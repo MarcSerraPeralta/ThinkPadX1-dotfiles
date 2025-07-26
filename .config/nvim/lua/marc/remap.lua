a18450c0d9f4a5f2dd0dc7f43d82d3ee3c672d88
MarcSerraPeralta <marcserraperalta@gmail.com>
2025-07-26 15:23:47 +0200

    Add more remaps to neovim

--- .config/nvim/lua/marc/remap.lua
+++ .config/nvim/lua/marc/remap.lua
@@ -1,2 +1,48 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- move selected lines up and down in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- cursor stays at the beginning of line when J
+vim.keymap.set("n", "J", "mzJ`z")
+-- cursor stays in the middle of the screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- copy to system's clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- paste on selected lines without changing buffer (so I can still paste again)
+vim.keymap.set("x", "<leader>p", [["_dP]])
+-- delete selected lines without changing buffer (so I don't loose my yanked text)
+vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
+
+-- change easily between projects with tmux
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux-sessionizer -s 0 --vsplit<CR>")
+vim.keymap.set("n", "<M-H>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
+vim.keymap.set("n", "<leader>f", function()
+    require("conform").format({ bufnr = 0 })
+end)
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>ca", function()
+    require("cellular-automaton").start_animation("make_it_rain")
+end)
+
+vim.keymap.set("n", "<leader><leader>", function()
+    vim.cmd("so")
+end)
