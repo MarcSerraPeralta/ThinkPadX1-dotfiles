836d091249b5bc30249f9900017e320a804420f8
MarcSerraPeralta <marcserraperalta@gmail.com>
2025-07-30 20:14:51 +0200

    Add telescope to Neovim

---
+++ .config/nvim/lua/marc/lazy/telescope.lua
@@ -0,0 +1,33 @@
+return {
+    "nvim-telescope/telescope.nvim",
+
+    tag = "0.1.5",
+
+    dependencies = {
+        "nvim-lua/plenary.nvim"
+    },
+
+    config = function()
+        require('telescope').setup({})
+
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        -- grep current word under cursor (no spaces)
+        vim.keymap.set('n', '<leader>pws', function()
+            local word = vim.fn.expand("<cword>")
+            builtin.grep_string({ search = word })
+        end)
+        -- grep current word (includes punctuation)
+        vim.keymap.set('n', '<leader>pWs', function()
+            local word = vim.fn.expand("<cWORD>")
+            builtin.grep_string({ search = word })
+        end)
+        -- prompted grep
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = vim.fn.input("Grep > ") })
+        end)
+        -- search across all neovim tags
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    end
+}
