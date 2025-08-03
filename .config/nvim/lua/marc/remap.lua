2f6cf01923bad4555e293e56e29656e35b3876b9
MarcSerraPeralta <marcserraperalta@gmail.com>
2025-08-03 16:36:33 +0200

    Remove <leader>pb keymap in neovim for <C-o>

--- .config/nvim/lua/marc/remap.lua
+++ .config/nvim/lua/marc/remap.lua
@@ -31,7 +31,4 @@ vim.keymap.set("n", "<leader>s", [[:%s/\\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left>
 -- makes the current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
--- go to previous buffer (useful when jumping to definitions with LSP)
-vim.keymap.set("n", "<leader>pb", function() vim.cmd("b#") end)
-
 -- the keymaps for the LSP are defined in init.lua
